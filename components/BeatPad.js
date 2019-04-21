f3c10324933cad7d48a22584bd2e0c6fbeb5d154
Brant Stuns <brantstuns@gmail.com>
2019-04-20 23:33:37 -0700

    Added first working sound integration 📣

--- components/BeatPad.js
+++ components/BeatPad.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
 import Sound from 'react-native-sound';
+import PropTypes from 'prop-types';
 Sound.setCategory('Playback');
 
+export default class BeatPad extends React.Component {
+  static propTypes = {
+    sound: PropTypes.object
+  };
 
-const BeatPad = props => {
-  return (
-    <View style={styles.beatPad}/>
-  );
+  state = {
+    drumSound: null
+  };
+
+  componentDidMount() {
+    const kickSound = new Sound('http://localhost:3000/sound/kick', null, error => {
+      if (error) {
+        console.log(error);
+      }
+      kickSound.play();
+    })
+    this.setState({
+      drumSound: this.props.drumSound || kickSound 
+    });
+  }
+
+  onBeatPadPress = () => {
+    this.state.drumSound.play();
+  };
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={this.onBeatPadPress}>
+        <View style={styles.beatPad} />
+      </TouchableWithoutFeedback>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -21,5 +49,3 @@ const styles = StyleSheet.create({
     marginBottom: 10
   }
 });
-
-export default BeatPad;
